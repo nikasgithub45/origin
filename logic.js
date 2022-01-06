0d243a9b392802291b6b13538e4583c9fb9731dd
nikasgithub45 <97154635+nikasgithub45@users.noreply.github.com>
2022-01-06 10:34:16 +0400

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,6 +1,6 @@
 const button = document.querySelector("a");
 const bg = document.querySelector("body");
-const colours = ["green","blue","black"];
+const colours = ["green","blue","black","white"];
 button.addEventListener("click",()=>{
     let rand = Math.floor(Math.random() * colours.length);
     bg.setAttribute("style","background:"+colours[rand]+"")
