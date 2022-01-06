029018dc7b2c6f8ac024f560563a7ebca62692ab
nikasgithub45 <n.onezashvili.exceedteam@gmail.com>
2022-01-06 15:21:41 +0400

    mycommit

--- logic.js
+++ logic.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const button = document.querySelector("a");
 const bg = document.querySelector("body");
 const colours = ["green","blue","black"];
@@ -5,3 +6,12 @@ button.addEventListener("click",()=>{
     let rand = Math.floor(Math.random() * colours.length);
     bg.setAttribute("style","background:"+colours[rand]+"")
 })
+=======
+const button = document.querySelector("a");
+const bg = document.querySelector("body");
+const colours = ["green","blue","black","white"];
+button.addEventListener("click",()=>{
+    let rand = Math.floor(Math.random() * colours.length);
+    bg.setAttribute("style","background:"+colours[rand]+"")
+})
+>>>>>>> 0d243a9b392802291b6b13538e4583c9fb9731dd
